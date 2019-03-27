ed098a9cf71ef848ba0b82385220c023afa3e115
Bruno Fernando <bfernandoco@gmail.com>
2019-03-27 03:31:55 -0300

    Teste - Diversos Maven Projects em um mesmo diretório

---
+++ ccp3anmca_exercicio01/src/main/java/br/usjt/arqsw/ccp3anmca_exercicio01/Ccp3anmcaExercicio01Application.java
@@ -0,0 +1,13 @@
+package br.usjt.arqsw.ccp3anmca_exercicio01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ccp3anmcaExercicio01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ccp3anmcaExercicio01Application.class, args);
+	}
+
+}
---
+++ ccp3anmca_hellospringboot/src/main/java/br/usjt/arqsw/ccp3anmca_hellospringboot/Ccp3anmcaHellospringbootApplication.java
@@ -0,0 +1,13 @@
+package br.usjt.arqsw.ccp3anmca_hellospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ccp3anmcaHellospringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ccp3anmcaHellospringbootApplication.class, args);
+	}
+
+}
